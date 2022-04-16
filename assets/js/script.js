7e53118128b9f9eeb4434ab54db0c6be7fc5efa9
dantocthang <tuongvi762@gmail.com>
2022-04-16 20:07:01 +0700

    add tutorial and products

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    AOS.init();
+    $('.tutorial__list').slick({
+        dots: true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button class="slider__button next"><i class="fa-solid fa-chevron-right"></i></button>',
+        nextArrow:'<button class="slider__button previous"><i class="fa-solid fa-chevron-left"></i></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
