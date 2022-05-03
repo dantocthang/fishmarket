f9fc2cd5609bd09c4a2b40c3ff6b6b23b96ff133
dantocthang <tuongvi762@gmail.com>
2022-05-03 10:18:01 +0700

    Responsive table

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -7,3 +7,10 @@ sidebarBtn.addEventListener('click', function(){
     else
         sidebar.classList.add('minimized')
 })
+
+window.addEventListener('resize', function(){
+    if (this.window.innerWidth<=739)
+        sidebar.classList.add('minimized')
+    else if (sidebar.classList.contains('minimized'))
+        sidebar.classList.remove('minimized')
+})
